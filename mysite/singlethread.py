ec8ef33b97588f195f628315050e6fa95245a245
MohamedShaban750 <36361652+MohamedShaban750@users.noreply.github.com>
2020-06-08 04:33:06 +0200

    Update singlethread.py

--- mysite/singlethread.py
+++ mysite/singlethread.py
@@ -1,7 +1,6 @@
 from .motor import motor
 from .fan import fan
 from .play import music
-from .sound import *
 def singlethread():
     # While server is running
     i = 0
@@ -9,7 +8,6 @@ def singlethread():
         motor()
         fan()
         music()
-        sound()
     # Once server is closed    
     GPIO.cleanup()        
 
